2d2500c381ab0101a72685cbc793103ebae120a9
Lars-Magnus Skog <ralphtheninja@riseup.net>
2023-01-23 01:30:43 +0100

    Basic input with a theme

--- demo/demo.js
+++ demo/demo.js
@@ -1,2 +1,2 @@
 const inputInteger = require('..')
-document.body.innerHTML = `<h1>${inputInteger()}</h1>`
+document.body.append(inputInteger())
--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
+const sheet = new CSSStyleSheet()
+const theme = getTheme()
+sheet.replaceSync(theme)
+
 function inputInteger () {
-  return 'input-integer'
+  const el = document.createElement('div')
+
+  const shadow = el.attachShadow({ mode: 'closed' })
+  shadow.innerHTML = `
+    <input></input>
+  `
+  shadow.adoptedStyleSheets = [sheet]
+
+  return el
+}
+
+function getTheme () {
+  return `
+    input {
+      padding: 8px 12px;
+      background-color: hsla(105, 100%, 85%, 1);
+      border-radius: 8px;
+    }
+    input:focus {
+      background-color: hsla(105, 100%, 60%, 1);
+    }
+  `
 }
 
 module.exports = inputInteger
